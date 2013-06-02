2689f6c3da6be6060ce287d5b7cce964bca90b99
Mathieu Dutto <dutto.mathieu@gmail.com>
2013-06-03 01:21:39 +0200

    domi content moyen content pas content

--- www/scripts/views/footerView.js
+++ www/scripts/views/footerView.js
@@ -20,6 +20,27 @@ define(
             render: function(){
                 html = "<section id='footer_container'><footer id='footer'></footer></section>";
                 this.$el.append(html);
+                    
+                var sentiment = self.setInterval(function(){ clock() }, 10000);
+                function clock(){
+                    if(localStorage.getItem('ip')){
+                        $.get('http://'+localStorage.getItem('ip')+':3000/sentiment')
+                        .success(function(data){
+                            var happyness = data.nivo,
+                                domi_happyness = $('#domi_happyness');
+                            if(happyness < 3){
+                                domi_happyness.addClass('icon-frown');
+                                domi_happyness.removeClass('icon-meh'), domi_happyness.removeClass('icon-smile');
+                            }else if(happyness < 5){
+                                domi_happyness.addClass('icon-meh');
+                                domi_happyness.removeClass('icon-frown'), domi_happyness.removeClass('icon-smile');
+                            }else{
+                                domi_happyness.addClass('icon-smile');
+                                domi_happyness.removeClass('icon-meh'), domi_happyness.removeClass('icon-frown');
+                            }
+                        })
+                    }
+                }
                 return this;
             }
             
